fb5fe535e0a0375407bcf6886396befacadb14b2
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-07 17:59:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,7 @@
 def key_for_min_value(name_hash)
     if name_hash.length == 0
       return nil
-    elsif a.sort_by { |key, value| value }.first
-      return name_hash
+    else
+      puts enu1.min { |a, b| a<=>b}
+
 end

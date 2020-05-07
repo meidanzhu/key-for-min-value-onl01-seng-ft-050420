976ad8d8c60df976036fb3979544b8e075c92d5e
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-07 18:04:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,19 @@
 def key_for_min_value(name_hash)
   if name_hash.length == 0
     return nil
+  else
+    array = []
+    name_hash.each do |key, value|
+      array << value
+    end
+    min_num = array[0]
+    array.each do |value|
+      min_num = value if value < min_num
+    end
+    name_hash.each do |key, value|
+      if min_num == value
+        return key
+      end
+    end
   end
-    hash.each do |k, v|
-  if v < lowest_value
-    lowest_value = v
-    lowest_key = k
-  end
-end
-    lowest_key
 end
